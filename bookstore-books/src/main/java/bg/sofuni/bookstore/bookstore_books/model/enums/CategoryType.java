a655d49849f3056a2c39204f2a026819ff1765be
Stanislava Germanova <114156419+StanislavaGermanovaa@users.noreply.github.com>
2024-07-24 23:20:42 +0300

    category

--- bookstore-books/src/main/java/bg/sofuni/bookstore/bookstore_books/model/enums/CategoryType.java
+++
@@ -1,5 +0,0 @@
-package bg.sofuni.bookstore.bookstore_books.model.enums;
-
-public enum CategoryType {
-    CRIME_STORY, HORROR, THRILLER, CLASSIC, LOVE_STORY, SCIENCE_FICTION, FANTASY, FAIRY_TALE
-}
--- bookstore-books/src/main/java/bg/sofuni/bookstore/bookstore_books/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.sofuni.bookstore.bookstore_books.repository;
-
-import bg.sofuni.bookstore.bookstore_books.model.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book,Long> {
-}
